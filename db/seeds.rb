ec0debccbb506d956c86c5bb0c3c2dbd7c11ae52
Leo Correa <lcorr005@gmail.com>
2016-02-01 17:13:20 -0500

    Saving point

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+puts "Creating projects"
+# 5.times do |i|
+#   p = Project.create(name: "Project #{i}", description: "Hello #{i}")
+#   if i.odd?
+#     p.entries.create(minutes: 10 * i, hours: 1 * i)
+#   else
+#     p.entries.create(minutes: i, hours: i)
+#   end
+# end
+
+p = Project.create(name: "Leo")
+10 .times do
+  p.entries.create(hours: 1, minutes: 10, date: 1.month.ago)
+end
